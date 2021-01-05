79cea486345394b2ce29004f4186b5977356fe99
Philip Bollen <PBollen@Ilionx.com>
2021-01-05 15:28:24 +0100

    feat(KNN model): add training function

--- src/Microphone/knn_classifier.py
+++ src/Microphone/knn_classifier.py
@@ -9,3 +9,14 @@ from sklearn.neighbors import KNeighborsClassifier as KNC
 def predict(knc_model: KNC, mfcc_sample: list):
     """returns a prediction from the given model"""
     return knc_model.predict([mfcc_sample])[0]
+
+def train_model(knc_model: KNC, training_data: list, training_classes: list):
+    """trains a given model with given data"""
+    known_classes = []
+    new_data = []
+    for i, training_class in enumerate(training_data):
+        for training_data in training_class:
+            known_classes.append(training_classes[i])
+            transformed_data = transform_data(training_data)
+            new_data.append(transformed_data)
+    knc_model.fit(new_data, known_classes)
