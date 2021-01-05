55dd979f1103fe8e59b5620b35e1625350017386
Philip Bollen <PBollen@Ilionx.com>
2021-01-05 15:01:53 +0100

    Add imports local imports

---
+++ src/limitless-gaming.py
@@ -0,0 +1,13 @@
+import sys
+from Camera.functions.open_face import *
+from Camera.functions.pi_cam import *
+from Camera.functions.my_functions import *
+try:
+    from GPIO.rgb_led import *
+    from GPIO.switch import *
+    GPIO_enabled = True
+except ImportError as err:
+    print(err)
+    GPIO_enabled = False
+from Microphone.knn_classifier import *
+from Microphone.microphone import *
