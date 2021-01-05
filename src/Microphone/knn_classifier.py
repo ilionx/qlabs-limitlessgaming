f38e79155f119fbc53481e7f2ec8c37286a86668
Philip Bollen <PBollen@Ilionx.com>
2021-01-05 15:27:42 +0100

    feat(KNN model): predict value

--- src/Microphone/knn_classifier.py
+++ src/Microphone/knn_classifier.py
@@ -5,3 +5,7 @@ import pickle
 import soundfile as sf
 from librosa import feature
 from sklearn.neighbors import KNeighborsClassifier as KNC
+
+def predict(knc_model: KNC, mfcc_sample: list):
+    """returns a prediction from the given model"""
+    return knc_model.predict([mfcc_sample])[0]
