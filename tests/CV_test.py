25f912b8d392670f79479654e2bee642a726ee1d
Philip Bollen <PBollen@Ilionx.com>
2021-01-06 16:25:56 +0100

    feat(CV-test): Add face and smile detection test

--- tests/CV_test.py
+++ tests/CV_test.py
@@ -17,5 +17,45 @@ class ComputerVisionTest(unittest.TestCase):
     FACE_CASCADE_MODEL = "haarcascade_frontalface_default.xml"
     SMILE_CASCADE_MODEL = "haarcascade_smile.xml"
 
+    def test_detect_face_on_picture(self):
+        "Test the detection of a face on a static picture"
+        image = cv2.imread(ASSETS_FOLDER + self.TRUMP_PHOTO, 0)
+        cascade_object = cv2.CascadeClassifier(
+            self.CASCADE_FOLDER + self.FACE_CASCADE_MODEL)
+        faces = []
+        for i in face_recognition.find_cascade(cascade_object, image, draw=True, draw_frame=image):
+            faces.append(i)
+        self.assertGreaterEqual(
+            len(faces), 1, "ERROR(test_detect_face_on_picture): No faces found")
+    def test_detect_smile_on_picture(self):
+        "Test the detection of a smile on a static picture"
+        image = cv2.imread(ASSETS_FOLDER + self.TRUMP_PHOTO_SMILE, 0)
+        cascade_object = cv2.CascadeClassifier(
+            self.CASCADE_FOLDER + self.SMILE_CASCADE_MODEL)
+        smiles = []
+        for i in face_recognition.find_cascade(cascade_object, image, draw=True, draw_frame=image):
+            smiles.append(i)
+        self.assertGreaterEqual(
+            len(smiles), 1, "ERROR(test_detect_smile_on_picture): No Smiles found")
+    def test_detect_face_and_smile_on_picture(self):
+        "Test the detection of a smile in a face on a static picture"
+        image = cv2.imread(ASSETS_FOLDER + self.TRUMP_PHOTO_SMILE, 0)
+        face_cascade_object = cv2.CascadeClassifier(
+            self.CASCADE_FOLDER + self.FACE_CASCADE_MODEL)
+        smile_cascade_object = cv2.CascadeClassifier(
+            self.CASCADE_FOLDER + self.SMILE_CASCADE_MODEL)
+        faces = []
+        smiles = []
+        for i in face_recognition.find_cascade(face_cascade_object, image, draw=True, draw_frame=image):
+            faces.append(i)
+            ROI = image[i[1]:i[1]+i[3], i[0]:i[0]+i[2]]
+            ROI = ROI[len(
+                ROI) // 2:]
+            for j in face_recognition.find_cascade(smile_cascade_object, ROI, draw=True, draw_frame=image, start_x=i[0], start_y=i[1] + len(ROI)):
+                smiles.append(j)
+        self.assertGreaterEqual(
+            len(faces), 1, "ERROR(test_detect_face_and_smile_on_picture): No faces found")
+        self.assertGreaterEqual(
+            len(smiles), 1, "ERROR(test_detect_face_and_smile_on_picture): No smiles found")
 if __name__ == "__main__":
     unittest.main()
