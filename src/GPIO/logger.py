b96b13fb37447fde74c88fdd1af7ff32ceb003f7
Philip Bollen <PBollen@Ilionx.com>
2021-01-07 10:40:40 +0100

    feat(log): Add raw logging method

--- src/GPIO/logger.py
+++ src/GPIO/logger.py
@@ -8,3 +8,19 @@ class Logger:
 
     Logger
     ===
+    """
+
+    def __init__(self, filename=None, time_format=None):
+        if filename is None:
+            self.filename = "log.csv"
+        else:
+            self.filename = filename
+        if time_format is None:
+            self.time_format = "unix"
+        else:
+            self.time_format = time_format
+
+    def _log_raw(self, message):
+        with open(self.filename, 'a') as log_file:
+            log_file.write(time())
+            log_file.write(message)
