8fbf20ed2f162ff36c3d0a8a1d9491532a4093ea
Philip Bollen <PBollen@Ilionx.com>
2021-01-07 13:59:58 +0100

    feat(status): Add class constructor

--- src/GPIO/status.py
+++ src/GPIO/status.py
@@ -3,3 +3,9 @@ import src.GPIO.rgb_led as rgb
 
 class Status:
     """This class handles a status events for the system"""
+    def __init__(self, status=None):
+        if status is None:
+            self.status = "Starting"
+        else:
+            self.status = status
+        self.lights = rgb.RGBled()
