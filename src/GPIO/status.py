177e15b50ecdf5214b70897765b01d6c7cbfc9b5
Philip Bollen <PBollen@Ilionx.com>
2021-01-07 14:00:27 +0100

    feat(status): Add status getter and setter

--- src/GPIO/status.py
+++ src/GPIO/status.py
@@ -9,3 +9,12 @@ class Status:
         else:
             self.status = status
         self.lights = rgb.RGBled()
+
+    def set_status(self, status):
+        if status.title() in self.possible_states:
+            self.status = status.title()
+        else:
+            raise Exception("Unknown status: %s" % status)
+
+    def get_status(self):
+        return self.status
