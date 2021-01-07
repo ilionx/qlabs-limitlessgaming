1aa054d96d0d74f2472a5dfa4a1eb9a71f95e04f
Philip Bollen <PBollen@Ilionx.com>
2021-01-07 14:01:22 +0100

    feat(status): Add predefined states

--- src/GPIO/status.py
+++ src/GPIO/status.py
@@ -3,6 +3,21 @@ import src.GPIO.rgb_led as rgb
 
 class Status:
     """This class handles a status events for the system"""
+    possible_states = (
+        "Starting",
+        "Idle",
+        "Running",
+        "Stopped",
+        "Restarting"
+    )
+    status_codes = {
+        "Starting": (0, 1, 0),
+        "Idle": (0, 0, 1),
+        "Running": (1, 1, 1),
+        "Stopped": (1, 0, 0),
+        "Restarting": (1, 0, 1)
+    }
+
     def __init__(self, status=None):
         if status is None:
             self.status = "Starting"
