2be50be2c4764a969dd1c23c09a9651051794adb
Philip Bollen <flip.philip@hotmail.com>
2021-04-07 20:01:16 +0200

    replace file parsing with json package

--- src/config/config_manager.py
+++ src/config/config_manager.py
@@ -1,4 +1,5 @@
 """load the configuration for the project"""
+import json
 
 
 class ConfigManager:
@@ -11,44 +12,33 @@ class ConfigManager:
         The relative path to the configuration file
     """
 
-    def __init__(self, filename):
-        if filename is None:
-            raise Exception("No configuration file found!")
-        self.filename = filename
-
-    def parse(self) -> dict:
-        """
-        Opens the configuration file and returns the configuration
-
-        Returns
-        -------
-        configuration:set
-            A set of configurations
-        """
-        with open(self.filename) as file:
-            lines = file.readlines()
-        configuration = {}
-        for line in lines:
-            config = line.strip().split(":")
-            configuration[config[0].strip()] = config[1].strip()
-        return configuration
-
-    def save(self, configuration, config_file=None) -> None:
-        """
-        Save the configuration
-
-        Parameters
-        ----------
-        configuration:set
-            A set of configurations to be saved
-        config_name:str
-            The name of the configuration
-        """
+    def __init__(self, configuration=None, filename=None) -> None:
         if configuration is None:
-            raise Exception(
-                "Expected a configuration, got {}".format(configuration))
-        if config_file is None:
-            config_file = self.filename
-        with open(config_file, mode="a") as file:
-            for x in configuration:
-                file.write(f"{x}:{configuration[x]}")
+            self.configuration = {}
+        else:
+            self.configuration = configuration
+        if filename is not None:
+            self.filename = filename
+        else:
+            self.filename = "Config.json"
+
+    def load(self, filename=None) -> dict:
+        if filename is None:
+            filename = self.filename
+        with open(filename, "r") as config_file:
+            self.configuration = json.load(config_file)
+
+    def save(self, filename=None, configuration=None) -> None:
+        if filename is None:
+            filename = self.filename
+        if configuration is None:
+            configuration = self.configuration
+
+        with open(filename, "w") as config_file:
+            json.dump(configuration, config_file)
+
+    def __getitem__(self, item):
+        return self.configuration[item]
+
+    def __call__(self, *args, **kwds) -> dict:
+        return self.configuration
