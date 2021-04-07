f268452fbdb4b20a9324e5591463e0ae80c98121
Philip Bollen <flip.philip@hotmail.com>
2021-04-07 19:58:20 +0200

    First Draft Web-UI

---
+++ src/UI/server/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, jsonify, render_template, redirect
+from flask.helpers import url_for
+from flask.wrappers import Response
+from data.plotting import create_plot
+from Camera.implementation import hsv_camera_feed
+PATH_TO_INDEX_PAGE = "../client"
+app = Flask(__name__,
+            static_url_path="",
+            static_folder=PATH_TO_INDEX_PAGE+"/static",
+            template_folder=PATH_TO_INDEX_PAGE)
+
+configuration_settings = [{"hueLow": 0, "hueHigh": 179,
+                           "saturationLow": 0, "saturationHigh": 255,
+                           "valueLow": 0, "valueHigh": 255}]
+
+
+@app.route("/")
+def start_page():
+    return redirect(url_for("index_page"))
+
+
+@app.route("/app/index")
+def index_page():
+    return render_template("index.html")
+
+
+@app.route("/app/dashboard")
+def dashboard_page():
+    bar = create_plot()
+    return render_template("Dashboard.html", plot=bar)
+
+
+@app.route("/app/configuration")
+def configuration_page():
+    return render_template("Configuration.html")
+
+
+@app.route("/api")
+def index_API():
+    return jsonify("Success")
+
+
+@app.route("/video/mask")
+def mask_video():
+    return Response(hsv_camera_feed(configuration_settings),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route("/api/data", methods=['GET', 'POST'])
+def data_API():
+    feature = request.args['selected']
+    graphJSON = create_plot(feature)
+    return graphJSON
+
+
+@app.route("/api/configuration")
+def configuration_API():
+    for arg in request.args:
+        configuration_settings[0][arg] = int(request.args.get(arg))
+    return jsonify(configuration_settings[0])
---
+++ src/UI/todo.md
@@ -0,0 +1,11 @@
+# UI To-Do list
+
+## Index-page
+general info
+links to other pages
+
+## Dashboard-page
+detailed info
+
+## Config-page
+Dials, buttons & sliders for the user to configure the system
