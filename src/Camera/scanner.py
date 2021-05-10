3401d2dc4e17558143bc9ecda5452e5bb8da2cda
Philip Bollen <flip.philip@hotmail.com>
2021-05-10 20:40:49 +0200

    Add contour and cascade scanner

--- src/Camera/scanner.py
+++ src/Camera/scanner.py
@@ -1,9 +1,10 @@
-from cv2 import (CHAIN_APPROX_SIMPLE, COLOR_BGR2HSV, RETR_EXTERNAL,
-                 contourArea, cvtColor, findContours, inRange)
+from cv2 import (CHAIN_APPROX_SIMPLE, COLOR_BGR2GRAY, COLOR_BGR2HSV,
+                 RETR_EXTERNAL, CascadeClassifier, contourArea, cvtColor,
+                 findContours, inRange)
 from numpy import array
 
 
-class Scanner:
+class ContourScanner:
     """A scanner class for finding contours
 
     Attributes
@@ -25,10 +26,26 @@ class Scanner:
         Filter the found contours based on size
     """
 
-    def __init__(self, lower_bound=(91, 122, 138), upper_bound=(101, 255, 212)):
+    def __init__(self,
+                 lower_bound=(91, 122, 138),
+                 upper_bound=(101, 255, 212)):
+        """a contour scanner
+
+        Parameters
+        ----------
+        lower_bound : tuple, optional
+            the default color to search for, by default (91, 122, 138)
+        upper_bound : tuple, optional
+            the default color to search for, by default (101, 255, 212)
+        """
         self.lower_bound = array(lower_bound)
         self.upper_bound = array(upper_bound)
         self.contours = []
+
+    def scan(self, frame):
+        self.find_contours(frame)
+        self.sort_contours()
+        return self.contours
 
     def find_contours(self, frame):
         """Find contours of the given settings inside the frame
@@ -69,3 +86,70 @@ class Scanner:
             else:
                 cnt_area = False
         self.contours = filtered_contour_list
+
+
+class CascadeScanner:
+    def __init__(self, cascade_object=None, scale_factor=2.5, min_neighbors=5):
+        """Create a Cascade Scanner
+
+        Parameters
+        ----------
+        cascade_object : str | ClassifierObject
+            a model used for object detection
+        scale_factor : float
+            the scale with which the image will be resized
+        min_neighbors : int
+            a parameter needed for the object detection
+        """
+        self.scale_factor = scale_factor
+        self.min_neighbors = min_neighbors
+        if cascade_object is not None:
+            if cascade_object is not str:
+                self.cascade_object = cascade_object
+
+            else:
+                self.cascade_object = CascadeClassifier(
+                    cascade_object)
+
+    def set_cascade_object(self, cascade_object):
+        if cascade_object is not str:
+            self.cascade_object = cascade_object
+
+        else:
+            self.cascade_object = CascadeClassifier(
+                cascade_object)
+
+    def convert(self) -> None:
+        self.frame = cvtColor(self.frame, COLOR_BGR2GRAY)
+
+    def detect(self):
+        self.objects = self.cascade_object.detectMultiScale(
+            self.frame, self.scale_factor, self.min_neighbors)
+
+    def get_objects(self):
+        object_list = []
+        for (x, y, width, height) in self.objects:
+            object_list.append(x, y, width, height)
+        return object_list
+
+    def read_frame(self, frame):
+        self.frame = frame
+
+    def scan(self, frame=None):
+        """scan a frame for a certain object
+
+        Parameters
+        ----------
+        frame : ,optional
+            the with the objects, by default None
+
+        Returns
+        -------
+        [(int,int,int,int),...]
+            the location of all the objects found in a frame
+        """
+        if frame is not None:
+            self.frame = frame
+        self.convert()
+        self.detect()
+        return self.get_objects()
