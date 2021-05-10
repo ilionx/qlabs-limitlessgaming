f1b5305f8f543040f423b388436f88fa55360ea6
Philip Bollen <flip.philip@hotmail.com>
2021-05-10 20:40:15 +0200

    simplefy config manager

--- src/config/config_manager.py
+++ src/config/config_manager.py
@@ -13,6 +13,14 @@ class ConfigManager:
     """
 
     def __init__(self, configuration=None, filename=None) -> None:
+        """
+        This class provides a simple way to load configuration files
+
+        Parameters
+        ----------
+        configuration: dict
+        filename: str
+        """
         if configuration is None:
             self.configuration = {}
         else:
@@ -20,25 +28,28 @@ class ConfigManager:
         if filename is not None:
             self.filename = filename
         else:
-            self.filename = "Config.json"
+            self.filename = "src/config.json"
+        self.load()
 
-    def load(self, filename=None) -> dict:
-        if filename is None:
-            filename = self.filename
-        with open(filename, "r") as config_file:
-            self.configuration = json.load(config_file)
+    def load(self) -> None:
+        with open(self.filename, "r") as config_file:
+            self.configuration.update(json.load(config_file))
 
-    def save(self, filename=None, configuration=None) -> None:
-        if filename is None:
-            filename = self.filename
-        if configuration is None:
-            configuration = self.configuration
+    def save(self) -> None:
+        with open(self.filename, "w") as config_file:
+            json.dump(self.configuration, config_file)
 
-        with open(filename, "w") as config_file:
-            json.dump(configuration, config_file)
-
-    def __getitem__(self, item):
+    def __getitem__(self, item) -> dict:
+        if item not in self.configuration:
+            return False
         return self.configuration[item]
 
     def __call__(self, *args, **kwds) -> dict:
         return self.configuration
+
+
+if __name__ == "__main__":
+    cfm = ConfigManager(filename="examples/sample.json")
+    print(cfm())
+    cfm = ConfigManager(filename="examples/Config.json", configuration={1: 2})
+    print(cfm())
